1db1c624349edf9f4022d08586ff544a775ae1ce
davi <kaliflamel@gmail.com>
2023-07-02 19:04:48 -0300

    deleting old files

--- src/database/migrations/20230701191317-users.js
+++ src/database/migrations/20230701191317-users.js
@@ -3,12 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+   await queryInterface.createTable('users', { 
+    id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement:true,
+    primaryKey: true,
+    } 
+  });
+
   },
 
   async down (queryInterface, Sequelize) {
